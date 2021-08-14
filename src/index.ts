f062200f20038a4e6b5dc3e8447d867b381efeff
Fei <33122873+pheihuihui@users.noreply.github.com>
2021-08-14 10:08:06 +0800

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { huobipro } from 'ccxt'
 import { Bot } from './bot'
-import { readFileSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 import { EOL } from 'os'
 
 function getKeys() {
-    let buff = readFileSync('C:\\Users\\optim\\Desktop\\key.txt')
-    let accessKey = buff.toString().split(EOL)[0].split('::')[1]
-    let secretKey = buff.toString().split(EOL)[1].split('::')[1]
-    return {
-        access: accessKey,
-        secret: secretKey
+    let txt = 'C:\\Users\\optim\\Desktop\\key.txt'
+    if (existsSync(txt)) {
+        let buff = readFileSync('C:\\Users\\optim\\Desktop\\key.txt')
+        let accessKey = buff.toString().split(EOL)[0].split('::')[1]
+        let secretKey = buff.toString().split(EOL)[1].split('::')[1]
+        return {
+            access: accessKey,
+            secret: secretKey
+        }
+    } else {
+        return {
+            access: 'accessKey',
+            secret: 'secretKey'
+        }
     }
 }
 
